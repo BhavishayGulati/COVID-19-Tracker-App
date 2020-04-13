b7fe2606cfdf808ec2f5391848e592b3fb44c56a
BhavishayGulati <54773402+BhavishayGulati@users.noreply.github.com>
2020-04-14 02:28:07 +0530

    home fragment added

---
+++ app/src/main/java/com/bhavishay/coronatracker/ui/home/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.bhavishay.coronatracker.ui.home
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.bhavishay.coronatracker.R
+
+class HomeFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.home_fragment, container, false)
+    }
+
+
+
+}
