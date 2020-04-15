dfaf41d5d554ba1c10323d8546b5d298aa1e98b3
BhavishayGulati <54773402+BhavishayGulati@users.noreply.github.com>
2020-04-15 20:35:56 +0530

    Merge branch 'master' of C:\Users\FUTURE FLIP\Desktop\AndroidCode\coronaTracker with conflicts.

--- app/src/main/java/com/bhavishay/coronatracker/activities/MainActivity.kt
+++ app/src/main/java/com/bhavishay/coronatracker/activities/MainActivity.kt
@@ -17,9 +17,11 @@ import com.bhavishay.coronatracker.ui.home.HomeFragment
 import com.bhavishay.coronatracker.ui.info.help.HelpFragment
 import com.bhavishay.coronatracker.ui.info.precautions.PrecautionsFragment
 import com.bhavishay.coronatracker.ui.news.NewsFragment
+import com.bhavishay.coronatracker.ui.news.ViewPagerAdapter
 import com.bhavishay.coronatracker.ui.states.StatesPage
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_news.*
 import kotlinx.android.synthetic.main.home_fragment.*
 import kotlinx.coroutines.*
 import java.lang.Exception
@@ -31,88 +33,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-
-
-        val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
-            when(menuItem.itemId){
-                R.id.nav_home ->{
-                    val fragment = HomeFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.container,fragment)
-                        .commit()
-                    return@OnNavigationItemSelectedListener true
+        val mOnNavigationItemSelectedListener =
+            BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.nav_home -> {
+                        val fragment = HomeFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
+                            .commit()
+                        return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.nav_stats -> {
+                        val fragment = StatesPage()
+                        supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
+                            .commit()
+                        return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.nav_news -> {
+                        val fragment = NewsFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
+                            .commit()
+                        return@OnNavigationItemSelectedListener true
+                    }
                 }
-                R.id.nav_stats ->{
-                    val fragment = StatesPage()
-                    supportFragmentManager.beginTransaction().replace(R.id.container,fragment)
-                        .commit()
-                    return@OnNavigationItemSelectedListener true
-                }
-                R.id.nav_news ->{
-                    val fragment = NewsFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.container,fragment)
-                        .commit()
-                    return@OnNavigationItemSelectedListener true
-                }
+                false
             }
-            false
-        }
 
         if (savedInstanceState == null) {
             val fragment = HomeFragment()
             supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
                 .commit()
         }
-        nav_view.setOnNavigationItemSelectedListener (mOnNavigationItemSelectedListener)
-
-//        nav_precautions.setOnClickListener{
-//            callPrecautionFragment()
-//        }
-
-//        nav_help.setOnClickListener{
-//            callHelpFragment()
-//        }
-
+        nav_view.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
-
-//    fun callPrecautionFragment(){
-//        val precautionsFragment = PrecautionsFragment()
-//        val fT = supportFragmentManager.beginTransaction()
-//        fT.add(R.id.container,precautionsFragment,"pre")
-//        fT.commit()
-//    }
-//
-//    fun callHelpFragment(){
-//        val fragment = HelpFragment()
-//        supportFragmentManager.beginTransaction().replace(R.id.container,fragment)
-//            .commit()
-//    }
-
-
-//    private fun testApi(){
-//        CoroutineScope(Dispatchers.IO).launch {
-//
-//            try {
-//
-//               val repository = WorldStatsRepository(
-//                   WorldStatsDatabase.getInstance(this@MainActivity),
-//               CountryStatsDatabase.getInstance(this@MainActivity)
-//               )
-//                val worldStats = repository.getWorldStats()
-//                Log.d("ApiResponse","total cases ${worldStats?.totalCases}")
-//
-////                delay(1000)
-////                val indiaStatsResponse = StatsApi.retrofitService.getIndiaStats()
-////                if(indiaStatsResponse.isSuccessful)
-////                    Log.d("ApiResponse","no of states ${indiaStatsResponse.body()?.states?.size} Total India Cases ${indiaStatsResponse.body()?.indiaTotalStat?.activeCases}")
-////                else Log.e("ApiResponse",indiaStatsResponse.errorBody()!!.string())
-////
-//                val newsApiResponse = NewsApi.retrofitService.getNews("covid india","2020-04-11",pageNo=1)
-//                if(newsApiResponse.isSuccessful)
-//                    Log.d("ApiResponse","status - ${newsApiResponse.body()?.status} totalNews - ${newsApiResponse.body()?.totalResults} news1 - ${newsApiResponse.body()?.articles?.get(0)?.newsTitle}")
-//                else Log.e("ApiResponse",newsApiResponse.errorBody()!!.string())
-//            }catch (e:Exception){
-//                Log.e("statsApiResponse",e.toString())
-//            }
-//        }
-//    }
 }
--- app/src/main/java/com/bhavishay/coronatracker/ui/states/StatesPage.kt
+++ app/src/main/java/com/bhavishay/coronatracker/ui/states/StatesPage.kt
@@ -1,18 +1,16 @@
 package com.bhavishay.coronatracker.ui.states
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.view.ContextMenu
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
 
 import com.bhavishay.coronatracker.R
-import com.bhavishay.coronatracker.repository.IndiaStatsRepository
-import com.bhavishay.coronatracker.repository.database.IndiaStatsDatabase
-import com.bhavishay.coronatracker.repository.database.StateStatsDatabase
-import kotlinx.android.synthetic.main.states_page_fragment.*
+import com.bhavishay.coronatracker.ui.countryList.CountryListFragment
 
 class StatesPage : Fragment() {
 
@@ -27,27 +25,17 @@ class StatesPage : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.states_page_fragment, container, false)
+
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(StatesPageViewModel::class.java)
-        // TODO: Use the ViewModel
-
-        viewModel.totalCases.observe(viewLifecycleOwner, Observer { totalCases ->
-            txt_cases.text = totalCases
-        })
-
-        viewModel.totalDeaths.observe(viewLifecycleOwner, Observer { totalDeaths ->
-            txt_deaths.text = totalDeaths
-        })
-
-        viewModel.totalRecovered.observe(viewLifecycleOwner, Observer { totalRecovered ->
-            txt_recovered.text = totalRecovered
-        })
-
-        viewModel.getStatesStat(IndiaStatsRepository(IndiaStatsDatabase.getInstance(context!!),
-            StateStatsDatabase.getInstance(context!!)))
+    @SuppressLint("ResourceType")
+    override fun onCreateContextMenu(
+        menu: ContextMenu,
+        v: View,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        val fragment = StatesPage()
+        activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.state_list,fragment)?.commit()
+        super.onCreateContextMenu(menu, v, menuInfo)
     }
-
 }
