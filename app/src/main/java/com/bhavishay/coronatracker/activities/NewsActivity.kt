7d5de36c41ba812cda5e95b86581b8a80654934f
Umang <umang.bhatt2000@gmail.com>
2020-04-15 21:28:10 +0530

    Nested scroll view in home fragment

--- app/src/main/java/com/bhavishay/coronatracker/activities/NewsActivity.kt
+++ app/src/main/java/com/bhavishay/coronatracker/activities/NewsActivity.kt
@@ -2,27 +2,16 @@ package com.bhavishay.coronatracker.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.lifecycle.*
 import com.bhavishay.coronatracker.R
-import com.bhavishay.coronatracker.ui.countryList.CountryListViewModel
-import com.bhavishay.coronatracker.ui.news.RumoursFragment
-import com.bhavishay.coronatracker.ui.news.ViewPagerAdapter
-import com.google.android.gms.common.api.internal.ActivityLifecycleObserver.of
-import com.google.firebase.components.Component.of
-import kotlinx.android.synthetic.main.activity_news.*
-import kotlinx.android.synthetic.main.precautions_fragment.*
-import java.time.DayOfWeek.of
-import java.util.EnumSet.of
-import java.util.Optional.of
 
 class NewsActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: CountryListViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
-        viewModel =  ViewModelProviders.of(this).get(CountryListViewModel::class.java)
+
 
 //        val fragmentAdapter = ViewPagerAdapter(supportFragmentManager)
 //        viewpager_id.adapter = fragmentAdapter
--- app/src/main/java/com/bhavishay/coronatracker/repository/WorldStatsRepository.kt
+++ app/src/main/java/com/bhavishay/coronatracker/repository/WorldStatsRepository.kt
@@ -6,6 +6,7 @@ import com.bhavishay.coronatracker.models.data.WorldTotalStats
 import com.bhavishay.coronatracker.repository.database.CountryStatsDatabase
 import com.bhavishay.coronatracker.repository.database.WorldStatsDatabase
 import com.bhavishay.coronatracker.repository.network.StatsApi
+import kotlinx.coroutines.delay
 import java.io.IOException
 import java.text.ParseException
 import java.text.SimpleDateFormat
@@ -30,7 +31,7 @@ class WorldStatsRepository(
         return worldTotalStats
     }
 
-    fun getCountriesStatsList():List<Country>?{
+     fun getCountriesStatsList(): List<Country>?{
         return getCountryStatsListFromLocalDatabase()
 
     }
@@ -69,6 +70,8 @@ class WorldStatsRepository(
 
     private fun getCountryStatsListFromLocalDatabase():List<Country>?{
         return countryStatsDatabase.countryStatsDatabaseDao.getAllCountries()
+
+
     }
 
     private fun addCountryStatsToLocalDatabase(countryStats: List<Country>?) {
@@ -89,7 +92,7 @@ class WorldStatsRepository(
                 worldTotalStats?.lastNetworkCallTime = formatter.format(Date())
                 Log.d("timeString"," refreshing ${formatter.format(Date())}")
 
-                    addWorldStatsToLocalDatabase(worldTotalStats)
+                addWorldStatsToLocalDatabase(worldTotalStats)
                 addCountryStatsToLocalDatabase(responseBody?.countriesStats)
 
 
