1ca6f40f8e341834463ae0f5205ba1413ebb8b00
BhavishayGulati <54773402+BhavishayGulati@users.noreply.github.com>
2020-04-16 09:54:22 +0530

    view pager implemented in MainActivity

--- app/src/main/java/com/bhavishay/coronatracker/activities/MainActivity.kt
+++ app/src/main/java/com/bhavishay/coronatracker/activities/MainActivity.kt
@@ -2,50 +2,69 @@ package com.bhavishay.coronatracker.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.bhavishay.coronatracker.R
 import com.bhavishay.coronatracker.ui.home.HomeFragment
 import com.bhavishay.coronatracker.ui.news.NewsFragment
+import com.bhavishay.coronatracker.ui.news.ViewPagerAdapter
 import com.bhavishay.coronatracker.ui.states.StatesPage
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var mViewPagerAdapter: ViewPagerAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mViewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+        viewPager.adapter = mViewPagerAdapter
+        viewPager.offscreenPageLimit = 3
+        title = "Corona Virus Tracker"
+
+        nav_view.setOnNavigationItemSelectedListener(this)
 
 
-        val mOnNavigationItemSelectedListener =
-            BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
-                when (menuItem.itemId) {
-                    R.id.nav_home -> {
-                        val fragment = HomeFragment()
-                        supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
-                            .commit()
-                        return@OnNavigationItemSelectedListener true
-                    }
-                    R.id.nav_stats -> {
-                        val fragment = StatesPage()
-                        supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
-                            .commit()
-                        return@OnNavigationItemSelectedListener true
-                    }
-                    R.id.nav_news -> {
-                        val fragment = NewsFragment()
-                        supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
-                            .commit()
-                        return@OnNavigationItemSelectedListener true
-                    }
-                }
-                false
-            }
-
-        if (savedInstanceState == null) {
-            val fragment = HomeFragment()
-            supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
-                .commit()
+//        val mOnNavigationItemSelectedListener =
+//            BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+//                when (menuItem.itemId) {
+//                    R.id.nav_home -> {
+//                        val fragment = HomeFragment()
+//                        supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
+//                            .commit()
+//                        return@OnNavigationItemSelectedListener true
+//                    }
+//                    R.id.nav_stats -> {
+//                        val fragment = StatesPage()
+//                        supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
+//                            .commit()
+//                        return@OnNavigationItemSelectedListener true
+//                    }
+//                    R.id.nav_news -> {
+//                        val fragment = NewsFragment()
+//                        supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
+//                            .commit()
+//                        return@OnNavigationItemSelectedListener true
+//                    }
+//                }
+//                false
+//            }
+//
+//        if (savedInstanceState == null) {
+//            val fragment = HomeFragment()
+//            supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
+//                .commit()
+//        }
+//        nav_view.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.nav_home -> viewPager.currentItem = 0
+            R.id.nav_stats-> viewPager.currentItem = 1
+            R.id.nav_news -> viewPager.currentItem = 2
         }
-        nav_view.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        return true
     }
 }
--- app/src/main/java/com/bhavishay/coronatracker/ui/news/ViewPagerAdapter.kt
+++ app/src/main/java/com/bhavishay/coronatracker/ui/news/ViewPagerAdapter.kt
@@ -13,30 +13,13 @@ class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm)
     {
         return when(position)
         {
-            0->{
-                HomeFragment()
-            }
-            1->{
-                StatesPage()
-            }
-            2 -> {
-                NewsFragment()
-            }
-            else->{
-                NewsFragment()
-            }
+            0-> HomeFragment()
+            1-> StatesPage()
+            2 -> NewsFragment()
+            else-> NewsFragment()
         }
     }
     override fun getCount(): Int {
-        return 2
-    }
-    override fun getPageTitle(position: Int): CharSequence? {
-        return when(position)
-        {
-            0-> "Home"
-            1 -> "Stats"
-            2 -> "News"
-            else-> "News"
-        }
+        return 3
     }
 }
